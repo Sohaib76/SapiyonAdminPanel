0a506e46f17520943e86cb46fbf284d6dde39db5
Sohaib <6sohaib@gmail.com>
2020-01-17 17:52:58 +0500

    Updated develop branch

---
+++ src/Pages/UsersPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ContactsInfoHeader from '../Components/ContactsInfoHeader.js'
+
+
+
+
+
+export default class UsersPage extends React.Component {
+    render(){
+        return(
+            <div>
+                     <ContactsInfoHeader/>
+                     <div className="main-screen-content">
+                       
+                     </div>
+                     
+            </div>
+           
+        )
+    }
+}
